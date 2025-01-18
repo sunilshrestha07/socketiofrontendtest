73cc97d0ecdea797abf3d813557bb6f6a0dfc620
Your Name <sunilcrestha9@gmai.com>
2025-01-18 13:55:04 +0545

    some chnages

--- src/app/captain/page.tsx
+++ src/app/captain/page.tsx
@@ -3,7 +3,7 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {useSelector} from 'react-redux';
 import {RootState} from '../redux/store';
-import {io,Socket} from 'socket.io-client';
+import {io, Socket} from 'socket.io-client';
 
 interface userSearchingDriverInterface {
   name: string;
@@ -23,19 +23,26 @@ interface userSearchingDriverInterface {
 
 export default function page() {
   const currentUser = useSelector((state: RootState) => state.user.currentUser);
-  const socketRef = useRef<Socket | null>(null)
+  const socketRef = useRef<Socket | null>(null);
   const driverId = currentUser?._id;
 
   //define the driver data
-  const driverData={
-    name:currentUser?.name,
-    id:currentUser?._id,
-    email:currentUser?.email,
-    location:{
-      latitude:0,
-      longitude:0,
-    }
-  }
+  const driverData = {
+    name: currentUser?.name,
+    id: currentUser?._id,
+    email: currentUser?.email,
+    phone: '9808048030',
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQBnleaDEx8-_IxWxO8lRPzYclZ67QGdUWRg&s',
+    location: {
+      latitude: 27.6332,
+      longitude: 85.5277,
+    },
+    vechicel: {
+      color: 'red',
+      plate: 'Ba 11 AA 1234',
+      vehicleType: 'Bike',
+    },
+  };
   const [userSearchingRide, setUserSearchingRide] = useState<userSearchingDriverInterface[]>([]);
 
   //useeffect for socket io connection
@@ -50,13 +57,12 @@ export default function page() {
     socket.on('broadCastDrives', ({formdata}) => {
       // if(currentDriverVehcilceType !== formdata.formdata.name) return
       setUserSearchingRide((prev) => [...prev, formdata.formdata]);
-      console.log('requesting user is',formdata.formdata);
+      console.log('requesting user is', formdata.formdata);
     });
 
     socket.on('rideAccepted', ({formdata}) => {
       alert(`ride accepted by user ${formdata.name}`);
       console.log('ride accepted by user ', formdata);
-
     });
 
     socket.on('terminateFindDriverRequest', ({formdata}) => {
@@ -75,22 +81,22 @@ export default function page() {
   }, []);
 
   const handelRequestRide = (userId: string) => {
-    if(socketRef.current===null) return
+    if (socketRef.current === null) return;
     socketRef.current.emit('requestRideToUser', {driverData, userId});
     console.log('made request to user', userId);
-
   };
   return (
     <>
       <div className="">
-        {userSearchingRide && userSearchingRide.map((data) => (
-          <div key={data.id}>
-            <p>{data.name}</p>
-            <div className="">
-              <button onClick={()=>handelRequestRide(data.id)}>Request Ride</button>
+        {userSearchingRide &&
+          userSearchingRide.map((data) => (
+            <div key={data.id}>
+              <p>{data.name}</p>
+              <div className="">
+                <button onClick={() => handelRequestRide(data.id)}>Request Ride</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
         <div className="">
           <h1>Hello captain</h1>
           <p>{currentUser?.name}</p>
